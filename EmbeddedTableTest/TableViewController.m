7e5a86ee5846076b73abf08cd49ec1b9be7f47f4
Simon Kågedal Reimer <simon.reimer@bontouch.com>
2015-11-07 22:44:02 +0100

    No hidden section, unwanted extra space

--- EmbeddedTableTest/TableViewController.m
+++ EmbeddedTableTest/TableViewController.m
@@ -27,32 +27,13 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (section == 1)
-        return 2;
-    else
-        return 0;
+    return 2;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    if (section == 1)
-        return @"Section";
-    else
-        return @"";
+    return @"Section";
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    if (section == 1)
-        return UITableViewAutomaticDimension;
-    else
-        return 0.1;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
-    if (section == 1)
-        return UITableViewAutomaticDimension;
-    else
-        return 0.1;
-}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class])
